140860c4b979d06c49b2233b5358eefcde643d3e
rakesh <rpeddamallu@juniper.net>
2023-06-24 12:50:22 +0530

    removed unused code

--- src/components/Home.tsx
+++ src/components/Home.tsx
@@ -1,9 +1,5 @@
-import React from 'react'
 import './Home.css'
-// import Card from './card'
 import CardsContainer from './CardsContainer'
-import SearchBar from './SearchBar'
-import Stats from './Stats'
 const Home = () => {
   return (
     <div className="home">
